de465eac9b1b594ca554ea4d28948e03e7cf7069
Witty Chang <wcc1213@gmail.com>
2017-09-14 09:06:08 -0700

    Edit layout and add onClickListener to 'Search Properties' button.

--- app/src/main/java/com/example/guest/willow/MainActivity.java
+++ app/src/main/java/com/example/guest/willow/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.guest.willow;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mAppNameTextView;
+    private Button mSearchPropertiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
         mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
         Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/CHOPS___.TTF");
         mAppNameTextView.setTypeface(appNameFont);
+
+        mSearchPropertiesButton = (Button) findViewById(R.id.searchPropertiesButton);
+            mSearchPropertiesButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Code to do property search here
+                    // Toast will just be an example to see that onclicklistener works
+                    Toast.makeText(MainActivity.this, "Get this done", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
