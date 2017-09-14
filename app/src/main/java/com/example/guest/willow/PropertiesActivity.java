a2868578cd0293e93d7e458692f70d6cf2e6edf2
Witty Chang <wcc1213@gmail.com>
2017-09-14 15:10:07 -0700

    Add code for Array Adapter and List View in Properties Activity.

--- app/src/main/java/com/example/guest/willow/PropertiesActivity.java
+++ app/src/main/java/com/example/guest/willow/PropertiesActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.Gravity;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 public class PropertiesActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,9 @@ public class PropertiesActivity extends AppCompatActivity {
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are the results for your property search in: " + location);
         mLocationTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, properties);
+        mListView.setAdapter(adapter);
     }
 }
