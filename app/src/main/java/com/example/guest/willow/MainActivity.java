d2a2169ca48120407dc940fb951405334b1e7a59
Witty Chang <wcc1213@gmail.com>
2017-09-14 09:27:11 -0700

    Add code to gather data from EditText component.

--- app/src/main/java/com/example/guest/willow/MainActivity.java
+++ app/src/main/java/com/example/guest/willow/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TextView mAppNameTextView;
+    private EditText mUserQuery;
     private Button mSearchPropertiesButton;
 
     @Override
@@ -21,10 +25,15 @@ public class MainActivity extends AppCompatActivity {
         Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/CHOPS___.TTF");
         mAppNameTextView.setTypeface(appNameFont);
 
+        mUserQuery = (EditText) findViewById(R.id.userQuery);
+
         mSearchPropertiesButton = (Button) findViewById(R.id.searchPropertiesButton);
             mSearchPropertiesButton.setOnClickListener(new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
+                    String location = mUserQuery.getText().toString();
+                    Log.d(TAG, location);
                     Intent intent = new Intent(MainActivity.this, PropertiesActivity.class);
                     startActivity(intent);
                     // Toast.makeText(MainActivity.this, "Get this done", Toast.LENGTH_LONG).show();
