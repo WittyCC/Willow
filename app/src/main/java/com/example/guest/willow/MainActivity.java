6562ffdf19e7bf7bd1cfdfe7b92f90582046173d
Witty Chang <wcc1213@gmail.com>
2017-09-16 22:59:40 -0700

    Add ButterKnife injection library and implement to refactor code.

--- app/src/main/java/com/example/guest/willow/MainActivity.java
+++ app/src/main/java/com/example/guest/willow/MainActivity.java
@@ -4,42 +4,43 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import android.widget.Toast;
+import android.util.Log;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     // public static final String TAG = MainActivity.class.getSimpleName();
-    private TextView mAppNameTextView;
-    private EditText mUserQuery;
-    private Button mSearchPropertiesButton;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind (R.id.userQuery) EditText mUserQuery;
+    @Bind(R.id.searchPropertiesButton) Button mSearchPropertiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
         Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/CHOPS___.TTF");
         mAppNameTextView.setTypeface(appNameFont);
 
-        mUserQuery = (EditText) findViewById(R.id.userQuery);
+        mSearchPropertiesButton.setOnClickListener(new View.OnClickListener() {
 
-        mSearchPropertiesButton = (Button) findViewById(R.id.searchPropertiesButton);
-            mSearchPropertiesButton.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    String location = mUserQuery.getText().toString();
-                    // Log.d(TAG, location);
-                    Intent intent = new Intent(MainActivity.this, PropertiesActivity.class);
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                    // Toast.makeText(MainActivity.this, "Get this done", Toast.LENGTH_LONG).show();
-                }
-            });
+            @Override
+            public void onClick(View v) {
+            String location = mUserQuery.getText().toString();
+            // Log.d(TAG, location);
+            Intent intent = new Intent(MainActivity.this, PropertiesActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            // Toast.makeText(MainActivity.this, "Get this done", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/guest/willow/PropertiesActivity.java
+++ app/src/main/java/com/example/guest/willow/PropertiesActivity.java
@@ -11,9 +11,12 @@ import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class PropertiesActivity extends AppCompatActivity {
-    private TextView mLocationTextView;
-    private ListView mListView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
 
     private String[] properties = new String[] {"7322 Blake Pl SW", "722 Nob Hill Ave N # D", "8249 Bagley Ave N", "4324 Greenwood Ave N # A", "725 14th Ave E", "899 NW Elford Dr", "3031 NW 65th St", "808 NW 125th St", "713 Martin Luther King Jr Way S", "459 Newton St", "1020 E Shelby St", "12701 8th Ave NW"};
 
@@ -21,14 +24,13 @@ public class PropertiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_properties);
+        ButterKnife.bind(this);
 
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are the results for your property search in: " + location);
         mLocationTextView.setGravity(Gravity.CENTER_HORIZONTAL);
 
-        mListView = (ListView) findViewById(R.id.listView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, properties);
         mListView.setAdapter(adapter);
 
