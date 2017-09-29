f3869d7cb486471520ceb12efd686dd418660c78
Witty Chang <wcc1213@gmail.com>
2017-09-29 08:39:51 -0700

    Add code for user log in status and log out.

--- app/src/main/java/com/example/guest/willow/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/willow/ui/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +18,8 @@ import android.widget.TextView;
 
 import com.example.guest.willow.Constants;
 import com.example.guest.willow.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,14 +31,11 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-//    private SharedPreferences mSharedPreferences;
-//    private SharedPreferences.Editor mEditor;
-
     private DatabaseReference mSearchedLocationReference;
-
     private ValueEventListener mSearchedLocationReferenceListener;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-    // public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
     @Bind (R.id.userQuery) EditText mUserQuery;
     @Bind(R.id.searchPropertiesButton) Button mSearchPropertiesButton;
@@ -66,15 +69,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//
-//        mEditor = mSharedPreferences.edit();
-
         Typeface appNameFont = Typeface.createFromAsset(getAssets(), "fonts/CHOPS___.TTF");
         mAppNameTextView.setTypeface(appNameFont);
 
         mSearchPropertiesButton.setOnClickListener(this);
         mSavedListingsButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
+
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
@@ -84,21 +138,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             saveLocationToFirebase(location);
 
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
-
-//        } else {
-//
-//        if(mUserQuery.getText().toString().length() == 0) {
-//                    mUserQuery.setError("Please enter a location");
-//        } else {
-//                    Log.d(TAG, location);
             Intent intent = new Intent(MainActivity.this, ListingRecordActivity.class);
             intent.putExtra("location", location);
             startActivity(intent);
         }
-            // Toast.makeText(MainActivity.this, "Get this done", Toast.LENGTH_LONG).show();
 
         if (v == mSavedListingsButton) {
             Intent intent = new Intent(MainActivity.this, SavedListingRecordActivity.class);
@@ -115,10 +158,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
     }
-
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
 }
 
 
